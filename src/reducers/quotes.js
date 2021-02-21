116cc8da202d6cbb80c6c3508e273c8094031d6c
satodd1967 <seth.todd@outlook.com>
2021-02-21 14:42:51 -0600

    commit tree 5c12947a1fa72315cdd4820116345f2a27d77b65

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,38 @@
+
+
 export default (state = [], action) => {
-  return state;
+
+    switch (action.type) {
+      case 'ADD_QUOTE':
+        // return [...state, { id: action.quote.id, content: action.quote.content, author: action.quote.author}];
+        return [...state, action.quote]
+    
+      case 'REMOVE_QUOTE':
+        return state.filter(quote => quote.id !== action.quoteId);
+        // id = state.findIndex(quote => quote.id  === action.id)
+        // return [...state.slice(0, id), ...state.slice(id + 1)];
+
+      case 'UPVOTE_QUOTE':
+        let downvoteState = state.map(quote => {
+          if (quote.id === action.quoteId) {
+            return { ...quote, votes: ++quote.votes}
+          } else {
+            return quote;
+          }
+        })
+        return downvoteState;
+
+      case 'DOWNVOTE_QUOTE':
+        let upvoteState = state.map(quote => {
+          if (quote.id === action.quoteId && quote.votes !== 0) {
+            return { ...quote, votes: --quote.votes}
+          } else {
+            return quote;
+          }
+        })
+        return upvoteState;
+    
+      default:
+        return state;
+  }
 }
